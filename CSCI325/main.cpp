4dd6fcfacaff4691cf342c9c7c94d43775480166
zane keller <zanxkell@ut.utm.edu>
2023-09-16 23:57:28 -0500

    added main.cpp

--- CSCI325/main.cpp
+++ CSCI325/main.cpp
@@ -1,43 +1,20 @@
 
 #include <iostream>
-
+#include "Vector.h"
 using namespace std;
+    
+    
+    
+        
+        
 
-int sum(int N){
-    int total=0;
     
-    for (int i=1; i<N+1; i++){
-        
-        //cout<<i<<endl;
-        total=total+i;
-        
-    }
-return total;
-    
-}
 
-
-int product(int N){
-    int total=1;
-    
-    for (int i=1; i<N+1; i++){
-        
-        //cout<<i<<endl;
-        total=total*i;
-        
-    }
-return total;
-    
-}
 
 
 int main() {
-     int N;
-    cout<<"please enter a number"<<endl;
-    cin>>N;
-    cout<<"here is the sum"<<endl;
-    cout<<sum(N)<<endl;
-    cout<<"here is the product"<<endl;
-    cout<<product(N)<<endl;
+  Vector b;
+
+  cout<<b.Size();
   return 0;
 }
